75a172b537c59c628d9dff6a24dcf324e9fb4212
User <User@DESKTOP-GS0BG09>
2024-11-06 18:46:06 +0200

    Add project files.

---
+++ TaskOne/TaskOne.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::cout << "Ïðèâ³ò Ñâ³òå!" << std::endl;
+
+    return 0;
+}
